37eba360d4a8ed094c5aaaf97ca4798c80ea30f1
FedericoIB <federicobrizuela93@hotmail.com>
2019-09-22 12:02:22 -0300

    principio signin

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Signin from "./views/Signin.vue";
 
 Vue.use(Router);
 
@@ -10,13 +10,13 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "signin",
+      component: Signin
     },
     {
       path: "/home",
-      name: "home",
-      component: Home
+      name: "signin",
+      component: Signin
     },
     {
       path: "/signup",
