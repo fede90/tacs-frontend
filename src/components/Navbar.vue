d54de6d66fe8a8b35aec7617524cc36dd8c033a7
frojo <rojo.fede@gmail.com>
2019-09-29 01:49:41 -0300

    oculto funciones del nav segun login

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,7 +16,7 @@
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
+      <b-navbar-nav class="ml-auto" v-if="userLoged">
         <b-nav-form>
           <b-form-input size="sm" class="mr-sm-2" placeholder="Search">
           </b-form-input>
